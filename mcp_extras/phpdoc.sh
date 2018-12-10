6fc371d844e23ed96b09e2f2a4fa0f00f33e5d82
andrea morello <andrea.morello@freetimers.com>
2018-12-10 10:52:28 +0100

    fix phpdoc script

---
+++ mcp_extras/phpdoc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export mcp_path="$(dirname $0)/../";
+export app_path="$(realpath $mcp_path )";
+if [ "$app_path" == "" ]; then
+    export app_path=$mcp_path ;
+fi;
+php $app_path/mcp_extras/phpdoc/phpDocumentor.phar -d $app_path/mcp/ -t $app_path/docs/api/
+php $app_path/mcp_extras/phpdoc/phpDocumentor.phar -d $app_path/mcp_modules -t $app_path/docs/api/modules
