445ff5fc922cc647aec45dbfee5fd829cbaf482c
LinHUniX Andrea Morello <andrea.morello@unix.team>
2019-05-02 21:43:40 +0100

    linhunix

---
+++ mcp/Pdo/Driver/mssqlDriver.php
@@ -0,0 +1,32 @@
+<?php
+namespace LinHUniX\Pdo\Driver;
+
+//use \LinHUniX\Pdo\Driver\pdoDriver;
+
+use \PDO;
+
+/*
+ * @copyright Content copyright to linhunix.com 2003-2018
+ * @author Andrea Morello <andrea.morello@linhunix.com>
+ * @version GIT:2018-v1
+ * this new class implement the PDO mode to connect on databases;
+ * @see [vendor]/mcp/Head.php  
+ */
+
+class mssqlDriver extends pdoDriver {
+
+
+    function __construct (\LinHUniX\Mcp\masterControlProgram &$mcp, array $scopeCtl, array $scopeIn)
+    {
+        $hostname = $scopeIn["hostname"];
+        $hostport = $scopeIn["hostport"];
+        $database = $scopeIn["database"];
+        $port="";
+        if (!empty($hostport)){
+            $port=";port=".$hostport;
+        }
+        $scopeIn["dburlcon"]='dblib:host=' . $hostname .$port. ';dbname=' . $database;
+        $scopeIn["options"]=array(PDO::ATTR_PERSISTENT => true);
+        parent::__construct($mcp,$scopeCtl, $scopeIn);
+    }
+}
