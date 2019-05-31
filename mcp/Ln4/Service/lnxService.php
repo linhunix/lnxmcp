75348b843edef621139e62b828baef6c2a40a316
Andrea <andrea.morello@freetimers.com>
2019-05-31 12:05:12 +0100

    create the logic of the new database

---
+++ mcp/Ln4/Service/lnxService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LinHUniX\Ln4\Service;
+
+use LinHUniX\Mcp\Model\mcpServiceModelClass;
+
+/**
+ * LinHUniX Web Application Framework.
+ *
+ * @author Andrea Morello <andrea.morello@linhunix.com>
+ * @copyright LinHUniX L.t.d., 2018, UK
+ * @license   Proprietary See LICENSE.md
+ *
+ * @version GIT:2018-v2
+ */
+class lnxService extends mcpServiceModelClass
+{
+    protected function moduleInit()
+    {
+        $this->spacename = __NAMESPACE__;
+        $this->classname = __CLASS__;
+    }
+}
