6c7c2e31884ff6b0b140ed84a17bdf4f2a73cbb9
Andrea <andrea.morello@freetimers.com>
2019-10-15 10:21:10 +0100

    upload step2

--- mcp/Tools/Func/Gfx.fnc.php
+++ mcp/Tools/Func/Gfx.fnc.php
@@ -8,11 +8,22 @@ function LnxUcm($scopein = null)
  * lnxmcpUpload function.
  *
  * @param array $scopein
- * -required category (for upload file )
+ *                       -required category (for upload file )
+ *                       -required fileconvert:
+ *                       --[rand]
+ *                       --[basename]
+ *                       --[filetype]
+ *                       --[field]
+ *                       --[category]
+ *                       --- all scopein field
+ *                       -required allowlist ext in the list is allowred
+ *
+ * @return array result
  */
 function lnxmcpUpload($scopein)
 {
     $mcpAdminModPath = lnxmcp()->getCfg('mcp.path').'/../mcp_modules/Upload/';
     lnxmcp()->setCfg('app.mod.path.LinHUniX.Upload', $mcpAdminModPath);
-    lnxmcp()->Controller('init',false,$scopein,'Upload',null,'LinHUniX');
+
+    return lnxmcp()->Controller('init', false, $scopein, 'Upload', null, 'LinHUniX');
 }
