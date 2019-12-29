ed2ca9b060aa8392ccfc4c08153f57480fb8e158
Andrea Morello <andrea.morello@freetimers.com>
2019-12-29 15:12:09 +0000

    apps

--- mcp/Tools/Func/Autoload.fnc.php
+++ mcp/Tools/Func/Autoload.fnc.php
@@ -45,3 +45,46 @@ function selfAutoLoad($srcPath)
     }
     spl_autoload_register('legacyAutoload', true);
 }
+
+
+/**
+ * Load on config the data of a specific module folder
+ * @param string $path
+ */
+function mcpLoadModPath($path) {
+    if (!is_dir($path)) {
+        lnxmcp()->warning($path." is not a directory");
+    }
+    $scannedItems = scandir($srcPath);
+    foreach ($scannedItems as $item) {
+        if ($item === '.' || $item === '..') {
+            continue;
+        }
+        $modfolder=$srcPath.DIRECTORY_SEPARATOR.$item;
+        if (is_dir($modfolder)) {
+            $modcfg=array();
+            if (file_exists($modfolder.DIRECTORY_SEPARATOR.'mcp.modules.json')){
+                $modcfg=lnxGetJsonFile($modfolder.DIRECTORY_SEPARATOR.'mcp.modules.json');
+            }
+            if(!isset($modcfg['vendor'])){
+                $modcfg['vendor']=lnxmcp()->getResource('def');
+            }
+            if(!isset($modcfg['module'])){
+                $modcfg['module']=$item;
+            }
+            $modset='app.mod.path.'.$modcfg['vendor'].'.'.$modcfg['module'];
+            lnxmcp()->setCfg($modset,$modfolder.DIRECTORY_SEPARATOR);
+            if(isset($modcfg['config'])){
+                if(is_array($modcfg['config'])){
+                    foreach ($modcfg['config'] as $ck=>$cv){
+                        lnxmcp()->setCfg($ck,$cv);
+                    }
+                }
+            }        
+            if(isset($modcfg['common'])){
+                if(is_array($modcfg['common'])){
+                    foreach ($modcfg['common'] as $ck=>$cv){
+                        lnxmcp()->setCommon($ck,$cv);
+                    }
+                }
+            }        
